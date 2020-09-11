e9c5b7b3575a7bd1cc7e99d1b0ef9e8a7ad85a18
Daniel Armas <daniel.armas9706@gmail.com>
2020-09-11 13:07:55 -0400

    Implementando una base de dato local para el home.

--- lib/src/data/repositories/remote/business_repository.dart
+++ lib/src/data/repositories/remote/business_repository.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:pamiksa/src/data/graphql/queries/queries.dart' as queries;
+import 'package:pamiksa/src/data/models/business.dart';
+import 'package:pamiksa/src/data/repositories/database_connection.dart';
+import 'package:sqflite/sqflite.dart';
 
 class BusinessRepository {
   final GraphQLClient client;
+  final DatabaseConnection _databaseConnection = DatabaseConnection();
+  static Database _database;
 
   BusinessRepository({@required this.client}) : assert(client != null);
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _databaseConnection.setDatabase();
+    return _database;
+  }
 
   Future<QueryResult> fetchBusiness() async {
     final WatchQueryOptions _options = WatchQueryOptions(
@@ -14,4 +25,23 @@ class BusinessRepository {
     );
     return await client.query(_options);
   }
+
+  //Insert data to Table
+  insert(table, data) async {
+    var connection = await database;
+    return await connection.insert(table, data);
+  }
+
+  //Clear database
+  clear() async {
+    var connection = await database;
+    await connection
+        .transaction((txn) async => txn.execute('DELETE FROM "Business"'));
+  }
+  //Get all records
+  /*Future<List<Map>> all() async {
+    var connection = await database;
+    return await connection.transaction(
+        (txn) async => await txn.rawQuery('SELECT * FROM "Business"'));
+  }*/
 }
