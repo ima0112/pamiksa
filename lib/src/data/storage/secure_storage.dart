88c2d05e18973c36de91822fedeb834c9878569d
ima0112 <grumpy0112@gmail.com>
2020-09-19 16:59:30 -0400

    Haciendo pub get

--- lib/src/data/storage/secure_storage.dart
+++ lib/src/data/storage/secure_storage.dart
@@ -1,28 +1,28 @@
-// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-// class SecureStorage {
-//   read(String key) async {
-//     final storage = new FlutterSecureStorage();
-//     return await storage.read(key: key);
-//   }
+class SecureStorage {
+  read(String key) async {
+    final storage = new FlutterSecureStorage();
+    return await storage.read(key: key);
+  }
 
-//   readAll(String key) async {
-//     final storage = new FlutterSecureStorage();
-//     return await storage.readAll();
-//   }
+  readAll(String key) async {
+    final storage = new FlutterSecureStorage();
+    return await storage.readAll();
+  }
 
-//   save(String key, value) async {
-//     final storage = new FlutterSecureStorage();
-//     await storage.write(key: key, value: value);
-//   }
+  save(String key, value) async {
+    final storage = new FlutterSecureStorage();
+    await storage.write(key: key, value: value);
+  }
 
-//   remove(String key) async {
-//     final storage = new FlutterSecureStorage();
-//     await storage.delete(key: key);
-//   }
+  remove(String key) async {
+    final storage = new FlutterSecureStorage();
+    await storage.delete(key: key);
+  }
 
-//   removeAll(String key) async {
-//     final storage = new FlutterSecureStorage();
-//     await storage.deleteAll();
-//   }
-// }
+  removeAll(String key) async {
+    final storage = new FlutterSecureStorage();
+    await storage.deleteAll();
+  }
+}
